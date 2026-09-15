be2dbf440c79bf0bb76ef9516e779ad05071e241
shamnad-sherief <sshamnad03@gmail.com>
2026-09-15 14:28:41 +0530

    dsa: initialize crate with longest substring example

---
+++ projects/dsa/examples/longest_substring.rs
@@ -0,0 +1,7 @@
+// longest substring without repeating characters
+// input : "abcabcbb"
+// output : 3
+// explanation : the longest substring without repeating characters is "abc" with the length of 3.
+fn main() {
+    let s = "abcabcbb";
+}
